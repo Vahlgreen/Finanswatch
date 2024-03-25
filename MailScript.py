5bd91c16e9054eb152cbd622550bc53233bfbf7b
Vahlgreen <vahlgreen@live.dk>
2024-03-25 11:39:53 +0100

    starter gratis prøve og sender mail

--- MailScript.py
+++ MailScript.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-
+from faker import Faker
 
 def SendEmail(subject,body,sender,recipients,pw):
     msg = MIMEText(body)
@@ -24,58 +24,75 @@ def SendEmail(subject,body,sender,recipients,pw):
         print("Email send")
 
 def StartTrial(mail,pw):
+    #create account url
     url = "https://finanswatch.dk/profile/create?redirectUrl=https%3A%2F%2Flogin.watchmedier.dk%2Fauth%2Frealms%2Fwatchmedier%2Fprotocol%2Fopenid-connect%2Fauth%3Fsite%3Dfinanswatch.dk%26ui_locales%3Dda%26scope%3Dopenid%2Bprofile%2Bemail%26response_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Ffinanswatch.dk%252Fauth%252Fcallback%253Fclient_name%253DKeycloakOidcClient_finanswatch.dk%26state%3Dc679a4b46c%26code_challenge_method%3DS256%26client_id%3Dwatch%26code_challenge%3DzVSfzKCs98mpivvxmfNqw8A7FrrdTo2wIEbITt0zmag"
-    # initiate browser
-    driver = webdriver.Edge()
-    driver.get(url)
-    time.sleep(1)
 
+    #initiate browser
+    options = webdriver.EdgeOptions()
+    options.add_argument("--headless=new")
+    driver = webdriver.Edge(options=options)
+    #driver.implicitly_wait(2) #Time.sleep virker bedre
+    #driver = webdriver.Edge()
+
+
+    #open browser
+    driver.get(url)
+
+    time.sleep(1)
+    #cookies
     try:
         driver.find_element(By.XPATH, "//*[@id='CybotCookiebotDialogBodyButtonDecline']").click()
     except:
-        print("Ingen cookies.. Fortsætter")
+        print("No cookies found")
+    fake = Faker()
+    fakeName = fake.name().split(" ")
 
+    print(fakeName)
+
+    #username
     driver.find_element(By.XPATH,'// *[ @ id = ":R59clnlttbq5la:"]').send_keys(mail)
     time.sleep(1)
+    #password
     driver.find_element(By.XPATH, '//*[@id=":R1aclnlttbq5la:"]').send_keys(pw)
     time.sleep(1)
-    driver.find_element(By.XPATH, '//*[@id=":R1bclnlttbq5la:"]').send_keys("peter")
+    #firstname
+    driver.find_element(By.XPATH, '//*[@id=":R1bclnlttbq5la:"]').send_keys(fakeName[0])
     time.sleep(1)
-    driver.find_element(By.XPATH, '//*[@id=":R1cclnlttbq5la:"]').send_keys("khan")
+    #lastname
+    driver.find_element(By.XPATH, '//*[@id=":R1cclnlttbq5la:"]').send_keys(" ".join(fakeName[1:]))
     time.sleep(1)
-    driver.find_element(By.XPATH, '//*[@id="createUserAcceptTerms"]').click()#
+    #accept terms
+    driver.find_element(By.XPATH, '//*[@id="createUserAcceptTerms"]').click()
     time.sleep(1)
+    #create account
     driver.find_element(By.XPATH, '/html/body/div[3]/div/main/div/div/div/div/form/button').click()
-
-
+    time.sleep(1)
+    #login page
     driver.get("https://finanswatch.dk/auth/login?redirectTo=https%3A%2F%2Ffinanswatch.dk%2F")
+
     try:
         driver.find_element(By.XPATH, "//*[@id='CybotCookiebotDialogBodyButtonDecline']").click()
     except:
-        print("Ingen cookies.. Fortsætter")
+        print("No cookies found")
 
     driver.find_element(By.XPATH, '// *[ @ id = "username"]').send_keys(mail)
     time.sleep(1)
     driver.find_element(By.XPATH, '// *[ @ id = "password"]').send_keys(pw)
+    time.sleep(1)
+    driver.find_element(By.XPATH, '// *[ @ id = "password"]').send_keys(Keys.ENTER)
 
-    #driver.find_element(By.XPATH, "// *[ @ id = 'kc-sign-up']").click()
+    #paywalled article
+    driver.get("https://finanswatch.dk/Finansnyt/Pengeinstitutter/article16930818.ece")
 
+    try:
+        driver.find_element(By.XPATH, "//*[@id='CybotCookiebotDialogBodyButtonDecline']").click()
+    except:
+        print("No cookies found")
+    time.sleep(1)
+    #start trial
+    driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div[2]/div[1]/div/main/div/div/article[1]/div[5]/div/div/article/ul/li[1]/button').click()
 
-    #//*[@id="createUserAcceptTerms"]
-    #/html/body/div[3]/div/main/div/div/div/div/form/button
-    #passwordElement = driver.find_element(By.XPATH,"//*[@id='password']")
-
-
-    #usernameElement.send_keys(mail)
-    #passwordElement.send_keys(pw)
-    #passwordElement.send_keys(Keys.ENTER)
-
-
-    #
-    #time.sleep(1)
-    #driver.find_element(By.XPATH, "//*[@id='__next']/div/div/header/div[1]/div/div/nav[2]/ul[2]/li[2]/a/span").click()
-
-    print("test")
+    driver.quit()
 def main():
 
     letters = ["a", "b", "c", "d", "e", "f", ".", "g", "h", "i", "j", "k", "l", "m", "n",
@@ -84,18 +101,22 @@ def main():
 
     emailCombos = list(itertools.combinations(letters, 20))
     fwEmail = "".join(random.sample(emailCombos, 1)[0]) + '@gmail.com'
-    pwCombos = list(itertools.combinations(numbers, 10))
+    pwCombos = list(itertools.combinations(numbers, 8))
     fwPassword = "".join(random.sample(pwCombos, 1)[0])
 
+    StartTrial(fwEmail,fwPassword)
 
     subject = "Nyt login til Finanswatch!"
-    body = f"Dit nye login kommer her \nbrugernavn: {fwEmail}\npassword: {fwPassword}"
-    sender = "frankfyr73@gmail.com"
-    pw = "ylmp xblq zcqj bqke"#app password
-    recipients = [sender, "vahlgreen@live.dk"]
+    body = f"Dit nye login kommer her \n brugernavn: {fwEmail}\n password: {fwPassword}"
 
-    StartTrial(fwEmail,fwPassword)
-    #SendEmail(subject,body,sender,recipients,pw)
+    with open("credentials.txt") as f:
+        lines = f.readlines()
+        sender = lines[0].strip()
+        recipients = list(lines[1].strip().split(","))
+        pw = lines[2].strip()
+
+
+    SendEmail(subject,body,sender,recipients,pw)
 
 
 main()
