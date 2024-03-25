0d6b4f80fbae02562f33f661554ed1742dcb67f9
Vahlgreen <vahlgreen@live.dk>
2024-03-25 20:04:44 +0100

    small changes

--- MailScript.py
+++ MailScript.py
@@ -10,7 +10,7 @@ import time
 from faker import Faker
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException
 
 def SendEmail(subject, body, sender, recipients, pw):
     msg = MIMEText(body)
@@ -28,42 +28,44 @@ def SendEmail(subject, body, sender, recipients, pw):
 
 
 def StartTrial(mail, pw):
-    # create account url
-    url = "https://finanswatch.dk/profile/create?redirectUrl=https%3A%2F%2Flogin.watchmedier.dk%2Fauth%2Frealms%2Fwatchmedier%2Fprotocol%2Fopenid-connect%2Fauth%3Fsite%3Dfinanswatch.dk%26ui_locales%3Dda%26scope%3Dopenid%2Bprofile%2Bemail%26response_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Ffinanswatch.dk%252Fauth%252Fcallback%253Fclient_name%253DKeycloakOidcClient_finanswatch.dk%26state%3Dc679a4b46c%26code_challenge_method%3DS256%26client_id%3Dwatch%26code_challenge%3DzVSfzKCs98mpivvxmfNqw8A7FrrdTo2wIEbITt0zmag"
-
-    # initiate browser
-
-    # Edge, virker lokalt, men ikke på pe server
+    # Edge, works locally
     # options = webdriver.EdgeOptions()
     # options.add_argument("--headless=new")
     # driver = webdriver.Edge(options=options)
     # driver.implicitly_wait(2) #Time.sleep virker bedre
 
-    # chrome, lokalt
+    # chrome, works locally
     # chrome_options = webdriver.ChromeOptions()
     # chrome_options.add_argument("--headless")
     # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=chrome_options)
 
+    # chrome, works on PE server
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=chrome_options)
 
+    # create account url
+    url = "https://finanswatch.dk/profile/create?redirectUrl=https%3A%2F%2Flogin.watchmedier.dk%2Fauth%2Frealms%2Fwatchmedier%2Fprotocol%2Fopenid-connect%2Fauth%3Fsite%3Dfinanswatch.dk%26ui_locales%3Dda%26scope%3Dopenid%2Bprofile%2Bemail%26response_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Ffinanswatch.dk%252Fauth%252Fcallback%253Fclient_name%253DKeycloakOidcClient_finanswatch.dk%26state%3Dc679a4b46c%26code_challenge_method%3DS256%26client_id%3Dwatch%26code_challenge%3DzVSfzKCs98mpivvxmfNqw8A7FrrdTo2wIEbITt0zmag"
+
     # open browser
     driver.get(url)
 
+    # implicitly wait function seems to crash
     time.sleep(1)
-    # cookies
+
+    # check for cookies
     try:
         driver.find_element(By.XPATH, "//*[@id='CybotCookiebotDialogBodyButtonDecline']").click()
     except:
-        print("No cookies found")
+        print("No cookies found. Script continues..")
+
+    #Faker object for name generation
     fake = Faker()
     fakeName = fake.name().split(" ")
 
-    print(fakeName)
-
+    #interacting with registration formula
     # username
     driver.find_element(By.XPATH, '// *[ @ id = ":R59clnlttbq5la:"]').send_keys(mail)
     time.sleep(1)
@@ -82,38 +84,40 @@ def StartTrial(mail, pw):
     # create account
     driver.find_element(By.XPATH, '/html/body/div[3]/div/main/div/div/div/div/form/button').click()
     time.sleep(1)
-    # login page
+
+    # Navigate to login page
     driver.get("https://finanswatch.dk/auth/login?redirectTo=https%3A%2F%2Ffinanswatch.dk%2F")
 
     try:
         driver.find_element(By.XPATH, "//*[@id='CybotCookiebotDialogBodyButtonDecline']").click()
     except:
-        print("No cookies found")
+        print("No cookies found. Script continues..")
 
+    # perform login
     driver.find_element(By.XPATH, '// *[ @ id = "username"]').send_keys(mail)
     time.sleep(1)
     driver.find_element(By.XPATH, '// *[ @ id = "password"]').send_keys(pw)
     time.sleep(1)
     driver.find_element(By.XPATH, '// *[ @ id = "password"]').send_keys(Keys.ENTER)
 
-    # paywalled article
+    # url for paywalled article
     driver.get("https://finanswatch.dk/Finansnyt/Pengeinstitutter/article16930818.ece")
 
     try:
         driver.find_element(By.XPATH, "//*[@id='CybotCookiebotDialogBodyButtonDecline']").click()
     except:
-        print("No cookies found")
+        print("No cookies found. Script continues..")
+
     time.sleep(1)
     # start trial
-    driver.find_element(By.XPATH,
-                        '//*[@id="__next"]/div/div/div[2]/div[1]/div/main/div/div/article[1]/div[5]/div/div/article/ul/li[1]/button').click()
+    driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div[2]/div[1]/div/main/div/div/article[1]/div[5]/div/div/article/ul/li[1]/button').click()
 
     driver.quit()
 
 
 def main():
-    # test symbols
-    # symbols = ["a","b", "c", "d", "e", "f", ".", "g", "h", "i", "j", "k", "l", "m", "n",
+    #test symbols
+    #symbols = ["a", "d", "e", "f", ".", "g", "h", "i", "j", "k", "l", "m", "n",
     #           "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y"]
     # prod symbols
     symbols = ["b", "c", "d", "e", "f", ".", "g", "h", "i", "j", "k", "l", "m", "n",
@@ -126,7 +130,11 @@ def main():
     pwCombos = list(itertools.combinations(numbers, 8))
     fwPassword = "".join(random.sample(pwCombos, 1)[0])
 
-    StartTrial(fwEmail, fwPassword)
+    try:
+        StartTrial(fwEmail, fwPassword)
+    except NoSuchElementException:
+        #in case mail was already taken, or something else went wrong
+        StartTrial(fwEmail, fwPassword)
 
     subject = "Nyt login til Finanswatch!"
     body = f"Dit nye login kommer her \n brugernavn: {fwEmail}\n adgangskode: {fwPassword} \n\n\n Github repo: https://github.com/Vahlgreen/Finanswatch"
